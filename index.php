68e80806a37a11c7401bdb01f9ebaa6728392d9e
alaaDragneel <moaalaa16@gmail.com>
2017-09-03 23:26:49 +0200

    implement laravel mix

--- index.php
+++ index.php
@@ -10,29 +10,4 @@ $file = new File(__DIR__);
 
 $app = new Application($file);
 
-
-$a = [
-    'ak1' => 'av1',
-    'ak2' => 'av2',
-    'ak3' => 'av3',
-    'ak4' => 'av4',
-    'ak5' => 'av5',
-];
-
-$b = [
-    'bk1' => 'bv1',
-    'bk2' => 'bv2',
-    'bk3' => 'bv3',
-    'bk4' => 'bv4',
-    'bk5' => 'bv5',
-];
-
-$object = $app;
-
-$c = [
-    'a' => &$a,
-    'b' => $b,
-    $object
-];
-
-dd($c, $file);
+dd($app);
